780364c488361f3168b45917446e3a7557be17cd
SlavaMarkovich <slavamarkovich@proton.me>
2026-07-01 23:17:14 +0300

    Add updated flake

---
+++ flake.nix
@@ -0,0 +1,55 @@
+{
+  description = "My system configuration";
+
+  inputs = {
+    # Обновлено до 26.05
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+
+    home-manager = {
+      # Обновлено до 26.05
+      url = "github:nix-community/home-manager/release-26.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: let
+    system = "x86_64-linux";
+    homeStateVersion = "26.05"; # Обновлено
+    user = "slava"; # Обновлено имя пользователя
+
+    # Список хостов сокращен до одного NixOwO
+    hosts = [
+      { hostname = "NixOwO"; stateVersion = "26.05"; }
+    ];
+
+    makeSystem = { hostname, stateVersion }: nixpkgs.lib.nixosSystem {
+      system = system;
+      specialArgs = {
+        inherit inputs stateVersion hostname user;
+      };
+      modules = [
+        # Обратите внимание: теперь путь будет ./hosts/NixOwO/configuration.nix
+        ./hosts/${hostname}/configuration.nix
+      ];
+    };
+  in {
+    # Генерация конфигурации NixOS
+    nixosConfigurations = nixpkgs.lib.foldl' (configs: host:
+      configs // {
+        "${host.hostname}" = makeSystem {
+          inherit (host) hostname stateVersion;
+        };
+      }) {} hosts;
+
+    # Конфигурация Home Manager (Standalone)
+    homeConfigurations.${user} = home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages.${system};
+      extraSpecialArgs = {
+        inherit inputs homeStateVersion user;
+      };
+      modules = [
+        ./home-manager/home.nix
+      ];
+    };
+  };
+}
