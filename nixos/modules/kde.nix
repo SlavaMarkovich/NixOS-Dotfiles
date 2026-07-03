fcba38b9a79b6370564ddb5f6bdb050d959d4607
SlavaMarkovich <slavamarkovich@proton.me>
2026-07-03 17:06:39 +0300

    Загальне покращення

--- nixos/modules/kde.nix
+++ nixos/modules/kde.nix
@@ -7,11 +7,8 @@
   services = {
     desktopManager.plasma6.enable = true;
 
-    displayManager.sddm = {
-      enable      = true;
-      wayland.enable = true;
-      autoNumlock = true;          # включить NumLock при входе
-      theme       = "breeze";      # тема по умолчанию KDE
+    displayManager = {
+      plasma-login-manager.enable = true;
     };
   };
 
